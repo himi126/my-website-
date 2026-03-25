8ac42e529b400cba3f4cf39e81ac08b7ac0f5d0e
himi <himi19990126@gmail.com>
2026-03-25 15:43:48 +0800

    Update AppContext.tsx

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { storage } from './lib/storage';
+import { storage } from './storage';
 import type { AppSettings, ModuleOrder } from '../types';
 
 interface AppContextType {
